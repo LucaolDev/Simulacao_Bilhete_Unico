d8a5beb9b6ea78b128694f02117ded118d3d403d
Luis Carlos <lucajunior2003@gmail.com>
2025-03-20 15:23:21 -0300

    commit

--- Ex02/src/Util.java
+++ Ex02/src/Util.java
@@ -2,63 +2,91 @@ import static javax.swing.JOptionPane.*;
 import static java.lang.Integer.parseInt;
 
 public class Util {
-
-    BilheteUnico[] bilhete = new BilheteUnico[5];
+    private Usuario[] usuarios = new Usuario[5];
     private int index = 0;
 
     public void menuPrincipal() {
-        int opcao = 0;
-        String menu = "1. Administrador\n2. Usuario\n3. Finalizar";
-
+        int opcao = 1;
+        String menu = """
+                1 - Administrador
+                2 - Usuário
+                3 - Finalizar
+                """;
         do {
-            opcao = parseInt(showInputDialog(menu));
-            if (opcao < 1 || opcao > 3) {
-                showMessageDialog(null, "Opção Inválida");
-            } else {
+            try {
+                opcao = parseInt(showInputDialog(menu));
                 switch (opcao) {
                     case 1:
-                        menuAdiministrador();
+                        menuAdmin();
                         break;
                     case 2:
-                        menuUsuario();
+                        break;
+                    case 3:
+                        showMessageDialog(null, "Encerrando o programa");
+                        break;
+                    default:
+                        showMessageDialog(null, "Opção inválida!");
                         break;
                 }
-            }
-
+            } catch(NumberFormatException e){}
         } while (opcao != 3);
     }
 
-    private void menuUsuario() {
-    }
-
-    private void menuAdiministrador() {
-        int opcao = 0;
-        String menuAdiministrador = "1. Emitir Bilhete\n2. Listar Bilhetes\n3. Remover Bilhetes\n4. Sair\n";
-
-        do{
-            opcao = parseInt(showInputDialog(menuAdiministrador));
-            if(opcao < 1 || opcao > 4){
-                showMessageDialog(null, "Opção Inválida");
-            }
-            else {
-                switch (opcao){
+    public void menuAdmin() {
+        int opcao = 1;
+        String menuAdmin = """
+                1 - Adicionar bilhete
+                2 - Remover bilhete
+                3 - Listar bilhetes registrados
+                4 - Sair
+                """;
+        do {
+            try {
+                opcao = parseInt(showInputDialog(menuAdmin));
+                switch (opcao) {
                     case 1:
-                        emitirBilhete();
+                        adicionarBilhete();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        showMessageDialog(null, "Opção inválida!");
                         break;
                 }
-            }
-        }while (opcao != 4);
+            } catch (NumberFormatException n) {}
+        } while(opcao != 4);
     }
 
-    private void emitirBilhete() {
-        String nome;
-        long cpf;
-        String perfil;
+//    private void removerBilhete() {
+//        int opcao;
+//        if (index > 0) {
+//            opcao = parseInt(showInputDialog("Selecione um bilhete para remover: "));
+//            if (opcao > index || opcao < 0) {
+//                showMessageDialog(null, "Usuário inválido");
+//            } else {
+//                usuarios[opcao] = usuarios[opcao + 1];
+//                showMessageDialog(null, "Usuário removido");
+//                index--;
+//            }
+//        } else {
+//            showMessageDialog(null, "Nenhum usuário para apagar");
+//        }
+//    }
 
-        if(index < bilhete.length){
-            nome = showInputDialog("Nome: ");
-
-
+    private void adicionarBilhete() {
+        if (index < usuarios.length) {
+            String nome = showInputDialog("Digite o nome do usuário");
+            long cpf = Long.parseLong(showInputDialog("Digite o CPF do usuário"));
+            String tipoTarifa = showInputDialog("Digite o tipo de usuário");
+            usuarios[index] = new Usuario(nome, cpf, tipoTarifa);
+            index++;
+            showMessageDialog(null, "Usuário cadastrado");
+        } else {
+            showMessageDialog(null, "Limite de bilhetes excedido");
         }
     }
 }
